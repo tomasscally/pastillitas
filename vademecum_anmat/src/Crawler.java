0821b048e54c34bb98a213ff34e928381adfd9b2
Tomas Scally <horizontedesucesos@gmail.com>
2012-08-21 12:12:34 -0300

    mas cosas

--- vademecum_anmat/src/Crawler.java
+++ vademecum_anmat/src/Crawler.java
@@ -14,13 +14,29 @@ public class Crawler extends SeleneseTestCase {
         for (String laboratorio :list){
             selenium.type("id=txt_laboratorio", laboratorio);
             selenium.click("id=btn_consultar");
+            waitForElement("Se ha utilizado el siguiente filtro para la búsqueda:");
             Integer cantPaginas = buscarCantidadPaginasLabo(laboratorio);
             if(cantPaginas!=0){
                 int i=1;
                 while(i < cantPaginas+1){
+                    //procesarpagina
+                    selenium.click("id=pagina_siguiente");
+
                     System.out.println(i);
                     i++;
                 }
+            }
+            selenium.click("id=div_volver");
+            selenium.waitForPageToLoad("30000");
+        }
+    }
+
+    private void waitForElement(String textContent) {
+        while(!selenium.getHtmlSource().contains(textContent)){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
     }
@@ -30,7 +46,7 @@ public class Crawler extends SeleneseTestCase {
         Integer ret = 0;
         if(selenium.isTextPresent("Página")){
             String paginaXdeY = selenium.getText("css=div.div_pagina_actual");
-            ret = new Integer(paginaXdeY.substring(paginaXdeY.indexOf("de")+3,paginaXdeY.length()));
+            ret = new Integer(paginaXdeY.substring(paginaXdeY.indexOf("de") + 3, paginaXdeY.length()));
         }
 
         return ret;
